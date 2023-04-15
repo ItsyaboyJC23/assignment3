b339c58d9a41874165dedb60cec92cb65764ea67
ItsyaboyJC23 <130813819+ItsyaboyJC23@users.noreply.github.com>
2023-04-14 23:33:03 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,43 @@
+var $ = Dom7;
+
+
+var app = new Framework7({
+  name: 'My App', // App name
+  theme: 'auto', // Automatic theme detection
+
+
+  el: '#app', // App root element
+
+
+  // App store
+  store: store,
+  // App routes
+  routes: routes,
+});
+// Login Screen Demo
+$('#my-login-screen .login-button').on('click', function () {
+  var username = $('#my-login-screen [name="username"]').val();
+  var password = $('#my-login-screen [name="password"]').val();
+
+  // Close login screen
+  app.loginScreen.close('#my-login-screen');
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br/>Password: ' + password);
+});
+
+/*** Geo location**/
+var x = document.getElementById("geo");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
